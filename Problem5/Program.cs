b80d2d699cb4cde1a0b97b2e77126689abc3732a
Вадим Малутин <malutin.vadim@yandex.ru>
2021-10-20 22:36:06 +0800

    оптимизировал решение

--- Problem5/Program.cs
+++ Problem5/Program.cs
@@ -13,7 +13,7 @@ namespace Problem5
             Console.Write("Введите размерность массива N: ");
             int N = Convert.ToInt32(Console.ReadLine());
             int[,] array = new int[N, N];
-            for (int i = 0; i < N; i++)
+            /*for (int i = 0; i < N; i++)
             {
                 if (i % 2 == 0)
                 {
@@ -34,7 +34,19 @@ namespace Problem5
                     }
                 }
                 Console.WriteLine();
-            }            
+            }*/
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                        array[i, j] = 1;
+                    else
+                        array[i, j] = 0;
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
     }
